84b77d430ebb44f297913c4c3c7273c7a907f89b
Bruce Oh <bruceo@beflex.co>
2019-05-09 15:23:22 +0900

    second commit: working on GET API

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const http  = require('http');
-const fs = requre('fs');
+const fs = require('fs');
 const _ = require('lodash');
+const express = require('express');
+const app = express();
+
 
 // testing a package
 //let example = _.fill([1,2,3,4,5], "banana",1,4);
 //console.log(example);
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+app.get('/example', (req,res) => {
+    res.send('hitting example route');
+});
+
+app.get('/example/:name/:age',(req,res)=> {
+    console.log(req.params);
+    res.send('example with route params');
+
+})
+app.listen(3000);
+
+/*
 const server = http.createServer((req, res) => {
+    
+
 
     //reading static resources
-    /*
+    
     const readStream = fs.createReadStream('./static/index.html');
     res.writeHead(200,{'Content-type' : 'text/html'});
-    readStream.pipe(res);*/
+    readStream.pipe(res);
+
 
     // check domain
-    /*
+    
     if(req.url === '/'){
         res.write('Hellow world from nodejs');
         res.end();
@@ -24,8 +45,11 @@ const server = http.createServer((req, res) => {
     else{
         res.write('using some other domain');
         res.end();
-    }*/
+    }
 });
 
+server.listen('3000');*/
 
-server.listen('3000');
+
+
+
